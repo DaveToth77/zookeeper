e4de5c4446c9f87192a23f4cfd3ef802e36fa7d5
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-09-28 07:42:29 -0400

    created web server

--- server.js
+++ server.js
@@ -41,14 +41,29 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results)
     }
     res.json(results);
-})
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+    res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now running on ${PORT}!`)

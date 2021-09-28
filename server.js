a4d249ec0114ffbfd6559c451b14a16a3e8eeef9
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-09-28 10:08:55 -0400

    implemented frontend

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({
 //parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -105,6 +107,20 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index oof the array will be
     req.body.id = animals.length.toString();

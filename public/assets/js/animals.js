93d8584073eb8394fba19cb9e7c53f359ffefca1
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-09-29 10:14:29 -0400

    configured database

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,78 +1,83 @@
-const $animalForm = document.querySelector('#animals-form');
-const $displayArea = document.querySelector('#display-area');
+<!DOCTYPE html>
+<html lang="en">
 
-const printResults = resultArr => {
-  console.log(resultArr);
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Zoo Keepr - Animals</title>
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Rubik:400,400i,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./assets/css/style.css" />
+</head>
 
-  const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
-    return `
-  <div class="col-12 col-md-5 mb-3">
-    <div class="card p-3" data-id=${id}>
-      <h4 class="text-primary">${name}</h4>
-      <p>Species: ${species.substring(0, 1).toUpperCase() + species.substring(1)}<br/>
-      Diet: ${diet.substring(0, 1).toUpperCase() + diet.substring(1)}<br/>
-      Personality Traits: ${personalityTraits
-        .map(trait => `${trait.substring(0, 1).toUpperCase() + trait.substring(1)}`)
-        .join(', ')}</p>
+<body>
+  <header class="py-4 mb-3">
+    <div class="flex-row justify-space-between align-center container-fluid mx-auto">
+      <h1 class="text-light bg-dark p-3">Zoo Keepr</h1>
+      <h3 class="text-light bg-dark p-3">A Zoo Animal Catalog</h3>
     </div>
-  </div>
-    `;
-  });
+  </header>
 
-  $displayArea.innerHTML = animalHTML.join('');
-};
+  <main class="container-fluid mx-auto mb-3">
+    <section class="p-3 mb-3 bg-dark flex-row align-center">
+      <h2 class="text-light col-12 col-lg-5">Our Animals.</h2>
+      <div class="col-12 col-lg-7 flex-row text-center justify-space-around align-center">
+        <a class="btn col-12 col-md-auto mx-2" href="/">Add To Our Catalog</a>
+        <a class="btn col-12 col-md-auto mx-2" href="/zookeepers">View Zookeepers</a>
+      </div>
+    </section>
 
-const getAnimals = (formData = {}) => {
-  let queryUrl = '/api/animals?';
+    <section class="my-2 flex-row justify-space-between">
+      <div class="col-12 col-lg-3 bg-dark p-3">
+        <h3 class="text-light">Filter Your Search.</h3>
+        <form id="animals-form">
+          <label for="diet-input" class="form-label">Diet</label>
+          <div class="flex-row justify-center">
+            <div class="col-auto mb-2">
+              <input type="radio" id="herbivore-input" name="diet" value="herbivore">
+              <label for="herbivore-input">Herbivore</label>
+            </div>
+            <div class="col-auto mb-2">
+              <input type="radio" id="folivore-input" name="diet" value="folivore">
+              <label for="folivore-input">Folivore</label>
+            </div>
+            <div class="col-auto mb-2">
+              <input type="radio" id="carnivore-input" name="diet" value="carnivore">
+              <label for="carnivore-input">Carnivore</label>
+            </div>
+            <div class="col-auto mb-2">
+              <input type="radio" id="omnivore-input" name="diet" value="omnivore">
+              <label for="omnivore-input">Omnivore</label>
+            </div>
+            <div class="col-auto mb-2">
+              <input type="radio" id="piscivore-input" name="diet" value="piscivore">
+              <label for="piscivore-input">Piscivore</label>
+            </div>
+          </div>
+          <label for="personality-input" class="form-label">Personality Traits (pick all that apply)</label>
+          <small>On macOS, use <kbd>command + click</kbd></small> <br />
+          <small>On Windows, use <kdb>ctrl + click</kdb></small>
+          <select multiple id="personality-input" class="form-input" name="personality">
+            <option value="hungry">Hungry</option>
+            <option value="zany">Zany</option>
+            <option value="anxious">Anxious</option>
+            <option value="loving">Loving</option>
+            <option value="goofy">Goofy</option>
+          </select>
 
-  Object.entries(formData).forEach(([key, value]) => {
-    queryUrl += `${key}=${value}&`;
-  });
+          <button class="btn">Get Animals</button>
+        </form>
+      </div>
+      <div class="col-12 col-lg-9 bg-dark p-3">
 
-  console.log(queryUrl);
+        <div class="flex-row justify-space-between" id="display-area"></div>
+      </div>
+    </section>
 
-  fetch(queryUrl)
-  .then(response => {
-    if (!response.ok) {
-      return alert('Error: ' + response.statusText);
-    }
-    return response.json();
-  })
-  .then(animalData => {
-    console.log(animalData);
-    printResults(animalData);
-  });
-};
 
-const handleGetAnimalsSubmit = event => {
-  event.preventDefault();
-  const dietRadioHTML = $animalForm.querySelectorAll('[name="diet"]');
-  let diet;
+  </main>
 
-  for (let i = 0; i < dietRadioHTML.length; i += 1) {
-    if (dietRadioHTML[i].checked) {
-      diet = dietRadioHTML[i].value;
-    }
-  }
+  <script src="./assets/js/animals.js"></script>
+</body>
 
-  if (diet === undefined) {
-    diet = '';
-  }
-
-  const personalityTraitArr = [];
-  const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions;
-
-  for (let i = 0; i < selectedTraits.length; i += 1) {
-    personalityTraitArr.push(selectedTraits[i].value);
-  }
-
-  const personalityTraits = personalityTraitArr.join(',');
-
-  const animalObject = { diet, personalityTraits };
-
-  getAnimals(animalObject);
-};
-
-$animalForm.addEventListener('submit', handleGetAnimalsSubmit);
-
-getAnimals();
+</html>
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,5 @@
 const $animalForm = document.querySelector('#animal-form');
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 
 const handleAnimalFormSubmit = event => {
   event.preventDefault();
@@ -25,26 +26,56 @@ const handleAnimalFormSubmit = event => {
     personalityTraits.push(selectedTraits[i].value);
   }
   const animalObject = { name, species, diet, personalityTraits };
-  
-fetch('/api/animals', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(animalObject)
-})
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    alert('Error: ' + response.statusText);
+
+  fetch('api/animals', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalObject)
   })
-  .then(postResponse => {
-    console.log(postResponse);
-    alert('Thank you for adding an animal!');
-  });
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding an animal!');
+    });
+};
+
+const handleZookeeperFormSubmit = event => {
+  event.preventDefault();
+
+  // get zookeeper data and organize it
+  const name = $zookeeperForm.querySelector('[name="zookeeper-name"]').value;
+  const age = parseInt($zookeeperForm.querySelector('[name="age"]').value);
+  const favoriteAnimal = $zookeeperForm.querySelector('[name="favorite-animal"]').value;
+
+  const zookeeperObj = { name, age, favoriteAnimal };
+  console.log(zookeeperObj);
+  fetch('api/zookeepers', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(zookeeperObj)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding a zookeeper!');
+    });
 };
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
-
+$zookeeperForm.addEventListener('submit', handleZookeeperFormSubmit);
